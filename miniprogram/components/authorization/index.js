4c563431c3565f8ee03afc8a6a825fcb4b52f86a
Jane <994824764@qq.com>
2023-07-07 23:18:38 +0800

    登录检测 - 封装组件 - 读取登录状态

--- miniprogram/components/authorization/index.js
+++ miniprogram/components/authorization/index.js
@@ -2,5 +2,19 @@ Component({
   data: {
     // 表示是否登录的标识
     isLogin: true,
+  },
+  lifetimes: {
+    attached() {
+      // 判断 token 是否存在
+      const isLogin = !!getApp().token // !!把token转成布尔值
+      this.setData({ isLogin })
+      // 存在：不理会
+      // 不存在：跳转到登录页面
+      if (!isLogin) {
+        wx.reLaunch({
+          url: '/pages/login/index'
+        })
+      }
+    }
   }
 })
