d16bf313e97b02932e266844cb5f6b343f01264b
Jane <994824764@qq.com>
2023-07-07 22:43:07 +0800

    用户管理 - 登录检测 - 获取token

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -4,5 +4,17 @@ import './utils/utils'
 // 导入 http 模块
 import './utils/http'
 App({
-  globalData: {},
+  onLaunch() {
+    // 打开页面时获取 token
+    this.getToken()
+  },
+  getToken() {
+    // 异步方式不会阻塞
+    wx.getStorage({
+      key: 'token',
+      success: ({ data }) => {
+        this.token = data
+      }
+    })
+  }
 })
