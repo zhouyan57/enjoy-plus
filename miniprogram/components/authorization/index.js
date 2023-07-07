adcfa5f25cdf731f0f02f29d922ff6d8fa12ce03
Jane <994824764@qq.com>
2023-07-07 23:27:49 +0800

    登录检测 - 封装组件 - 保存未访问的页面

--- miniprogram/components/authorization/index.js
+++ miniprogram/components/authorization/index.js
@@ -11,8 +11,15 @@ Component({
       // 存在：不理会
       // 不存在：跳转到登录页面
       if (!isLogin) {
+        // 得到未访问的页面路径
+        // 1. 得到页面栈
+        const pageStack = getCurrentPages()
+        // 2. 页面栈中的最后一个成员就是未访问的页面：在页面中得到它的路径信息
+        const currentPage = pageStack[pageStack.length - 1]
+        // 3. 保存到登陆页面的路径上
+        const redirectURL = currentPage.route
         wx.reLaunch({
-          url: '/pages/login/index'
+          url: `/pages/login/index?redirectURL=${redirectURL}`
         })
       }
     }
