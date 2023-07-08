a2dcd1a4a7afa73761416f778aff93e974e38dcf
Jane <994824764@qq.com>
2023-07-08 17:59:07 +0800

    个人资料 - 获取设置头像

--- miniprogram/pages/profile/index.js
+++ miniprogram/pages/profile/index.js
@@ -12,5 +12,32 @@ Page({
     this.setData({
       'userInfo.nickName': nickName
     })
+  },
+  // 获取头像
+  getUserAvatar(ev) {
+    this.updateUserAvatar(ev.detail.avatarUrl)
+  },
+  // 上传头像
+  updateUserAvatar(avatarUrl) {
+    wx.uploadFile({
+      url: wx.http.baseURL + '/upload',
+      filePath: avatarUrl, // 小程序生成的临时地址 外界不能访问到
+      name: 'file',
+      formData: {
+        type: 'avatar',
+      },
+      header: {
+        Authorization: getApp().token,
+      },
+      success: (res) => {
+        // 得到数据
+        const { code, data } = JSON.parse(res.data)
+        if (code !== 10000) return wx.utils.toast()
+        // 保存数据
+        this.setData({
+          'userInfo.avatar': data.url
+        })
+      }
+    })
   }
 })
