055932d0a239af3115c3b7da7db2199ff1b7b289
Jane <994824764@qq.com>
2023-07-08 16:09:49 +0800

    个人资料 - 获取设置昵称

--- miniprogram/pages/profile/index.js
+++ miniprogram/pages/profile/index.js
@@ -1,14 +1,16 @@
 Page({
-  onLoad() {
-    // 进入页面时判断 token 是否存在
-    // 1. 得到 token
-    const token = getApp().token
-    // 2. 判断 token
-    if (!token) {
-      // 未登录：跳转到登录页面
-      wx.reLaunch({
-        url: '/pages/login/index'
-      })
-    }
+  // 获取输入的昵称
+  getUserNickName(ev) {
+    this.updateNickName(ev.detail.value)
+  },
+  // 修改昵称
+  async updateNickName(nickName) {
+    // 将内容提交到服务器，去修改昵称
+    const { code } = await wx.http.put('/userInfo', { nickName })
+    if (code !== 10000) return wx.utils.toast()
+    // 保存昵称
+    this.setData({
+      'userInfo.nickName': nickName
+    })
   }
 })
