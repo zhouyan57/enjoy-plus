0158e2f2df452d6e6d0d04ee14bb9629a9cd650f
Jane <994824764@qq.com>
2023-07-08 09:39:50 +0800

    登录注册 - 验证表单数据

--- miniprogram/pages/login/index.js
+++ miniprogram/pages/login/index.js
@@ -3,11 +3,14 @@ let secret_code = ''
 Page({
   data: {
     // 手机号
-    mobile: '',
+    mobile: '13700000000',
+    // 验证码
+    code: '',
     countDownVisible: false,
   },
 
   countDownChange(ev) {
+    // ev.detail:   day 0 hour 0 minutes 1 seconds 0
     this.setData({
       timeData: ev.detail,
       countDownVisible: ev.detail.minutes === 1 || ev.detail.seconds > 0,
@@ -21,6 +24,14 @@ Page({
     const valid = reg.test(this.data.mobile.trim())
     // 判断结果
     if (!valid) wx.utils.toast('手机号不合法')
+    return valid
+  },
+  // 验证验证码
+  verifyCode() {
+    // 定义验证码的正则
+    const reg = /^\d{6}$/
+    const valid = reg.test(this.data.code.trim())
+    if (!valid) wx.utils.toast('验证码不合法')
     return valid
   },
   // 获取验证码的方法
@@ -38,8 +49,16 @@ Page({
   },
   // 点击下方文本时，将验证码复制到剪贴板中
   copyCode() {
+    if (!secret_code) return
     wx.setClipboardData({
       data: secret_code
     })
+  },
+  // 完成登录逻辑
+  submitForm() {
+    // 1. 验证参数的合法性
+    if (!this.verifyMobile()) return
+    if (!this.verifyCode()) return
+
   }
 })
