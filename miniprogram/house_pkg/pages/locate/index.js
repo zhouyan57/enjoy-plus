9eb5e71ed70fb2ff46ba60bee7fee9512e1eba7a
Jane <994824764@qq.com>
2023-07-09 11:12:39 +0800

    腾讯位置服务 - 逆地址解析(经纬度->文字信息)

--- miniprogram/house_pkg/pages/locate/index.js
+++ miniprogram/house_pkg/pages/locate/index.js
@@ -1,4 +1,5 @@
 // house_pkg/pages/locate/index.ts
+import qqMap from '../../../utils/qqmap'
 Page({
   onLoad() {
     // 自动获取定位
@@ -8,10 +9,22 @@ Page({
   async getLocation() {
     const { longitude, latitude } = await wx.getLocation()
     // 保存位置信息
-    console.log(latitude, longitude);
+    this.getPoint(latitude, longitude)
   },
+  // 手动获取定位
   async chooseLocation() {
     const { longitude, latitude } = await wx.chooseLocation()
-    console.log(latitude, longitude);
+    this.getPoint(latitude, longitude)
+  },
+  // 将经纬度转为位置描述
+  getPoint(latitude, longitude) {
+    qqMap.reverseGeocoder({
+      location: [latitude, longitude].join(','),
+      success: ({ result: { address } }) => {
+        // 结果为当前所在的地址
+        this.setData({ address })
+        console.log(address);
+      }
+    })
   }
 })
