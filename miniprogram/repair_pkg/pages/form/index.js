d50a95a3a0ae9079496917d1536d9cd7f0d36edf
Jane <994824764@qq.com>
2023-07-09 21:31:59 +0800

    在线报修 - 报修房屋(选择列表中房屋后，将数据反显到表单上并改变样式)

--- miniprogram/repair_pkg/pages/form/index.js
+++ miniprogram/repair_pkg/pages/form/index.js
@@ -4,17 +4,40 @@ Page({
     houseLayerVisible: false,
     repairLayerVisible: false,
     dateLayerVisible: false,
-    houseList: [
-      { name: '北京西三旗花园1号楼 101' },
-      { name: '北京东村家园3号楼 302' },
-      { name: '北京育新花园3号楼 703' },
-      { name: '北京天通苑北苑8号楼 403' },
-    ],
+    houseList: [],
     repairItem: [{ name: '水路卫浴' }, { name: '电路灯具' }, { name: '管道疏通' }, { name: '开锁换锁' }],
     fileList: [
       { url: '/repair_pkg/static/uploads/attachment.jpg' },
       { url: '/repair_pkg/static/uploads/attachment.jpg' },
     ],
+  },
+  // 打开页面时要获取数据源
+  onLoad() {
+    // 获取报修房屋的数据源
+    this.getHouseList()
+  },
+  // 获取报修房屋的数据源
+  async getHouseList() {
+    const { code, data: houseList } = await wx.http.get('/house')
+    if (code !== 10000) return wx.utils.toast()
+    if (houseList.length === 0) {
+      // wx.utils.toast('对不起，您没有可报修的房屋')
+      wx.showToast({
+        title: '对不起，您没有可报修的房屋',
+        icon: 'none',
+        duration: 2000,
+        complete: () => {
+          setTimeout(() => {
+            wx.navigateBack()
+          }, 2000)
+        }
+      })
+    }
+    this.setData({ houseList })
+  },
+  // 选中房屋选项的成员时，会触发
+  selectHouse(ev) {
+    this.setData({ houseInfo: ev.detail.name })
   },
   openHouseLayer() {
     this.setData({ houseLayerVisible: true })
