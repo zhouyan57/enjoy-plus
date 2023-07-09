81007624556dd4e7ef9287913bb4a3727e7dabf2
Jane <994824764@qq.com>
2023-07-09 09:24:54 +0800

    我的 - 获取用户资料

--- miniprogram/pages/my/index.js
+++ miniprogram/pages/my/index.js
@@ -4,4 +4,23 @@ Page({
       url: '/pages/login/index',
     })
   },
+  onLoad() {
+    this.getUserInfo()
+  },
+  // 获取用户信息
+  async getUserInfo() {
+    try {
+      const { code, data } = await wx.http.get('/userInfo')
+      if (code !== 10000) return wx.utils.toast()
+      // 保存数据
+      this.setData({
+        userInfo: {
+          avatar: data.avatar,
+          nickName: data.nickName
+        }
+      })
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 })
