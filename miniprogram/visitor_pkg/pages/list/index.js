02b151b102b69f079957793f09d174f8744200f3
Jane <994824764@qq.com>
2023-07-10 15:19:28 +0800

    我的访客 - 数据渲染

--- miniprogram/visitor_pkg/pages/list/index.js
+++ miniprogram/visitor_pkg/pages/list/index.js
@@ -1,7 +1,18 @@
 Page({
-  goPassport() {
+  onLoad() {
+    // 获取访客列表
+    this.getVistorList()
+  },
+  async getVistorList() {
+    const { code, data: { rows: visitorList } } = await wx.http.get('/visitor')
+    if (code !== 10000) return wx.utils.toast('获取访客列表失败!')
+    this.setData({ visitorList })
+  },
+  goPassport(ev) {
+    const { id, status } = ev.detail
+    if (status === 0) return wx.utils.toast('当前通行证已失效')
     wx.navigateTo({
-      url: '/visitor_pkg/pages/passport/index',
+      url: '/visitor_pkg/pages/passport/index?id=' + id,
     })
   },
 })
