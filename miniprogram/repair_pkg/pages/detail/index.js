228dd36b79e7a133692d1e973fd07933984dbd46
Jane <994824764@qq.com>
2023-07-10 10:03:01 +0800

    我的报修 - 取消报修

--- miniprogram/repair_pkg/pages/detail/index.js
+++ miniprogram/repair_pkg/pages/detail/index.js
@@ -3,6 +3,7 @@ Page({
   data: {
     latitude: 40.060539,
     longitude: 116.343847,
+    dialogVisible: false
   },
   onLoad({ id }) {
     this.getRepairDetail(id)
@@ -13,5 +14,22 @@ Page({
     console.log(repairDetail);
     if (code !== 10000) return wx.utils.toast()
     this.setData({ ...repairDetail })
-  }
+  },
+  // 取消报修
+  cancelRepair() {
+    // 面板打开
+    this.setData({ dialogVisible: true })
+  },
+  // 关闭面板
+  async dialogClose(ev) {
+    if (ev.detail === 'confirm') {
+      // 发送请求到服务器将当前维修取消
+      const { code } = await wx.http.put(`/cancel/repaire/${this.data.id}`)
+      if (code !== 10000) return wx.utils.toast()
+      // 将当前报修状态设置为取消中
+      this.setData({
+        status: 0,
+      })
+    }
+  },
 })
