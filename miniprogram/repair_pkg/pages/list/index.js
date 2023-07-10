ebfac7d891d82540026797bf1a168dcd4eb39cad
Jane <994824764@qq.com>
2023-07-10 08:55:26 +0800

    我的报修 - 列表页面渲染

--- miniprogram/repair_pkg/pages/list/index.js
+++ miniprogram/repair_pkg/pages/list/index.js
@@ -1,4 +1,15 @@
 Page({
+  onShow() {
+    // 获取报修列表
+    this.getRepairList()
+  },
+  async getRepairList() {
+    const { code, data: { rows: repairList } } = await wx.http.get('/repair', { current: 1, pageSize: 10 })
+    if (code !== 10000) return wx.utils.toast('获取报修列表失败!')
+    this.setData({
+      repairList
+    })
+  },
   goDetail() {
     wx.navigateTo({
       url: '/repair_pkg/pages/detail/index',
