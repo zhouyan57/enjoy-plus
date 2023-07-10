908a94c2f41fcdd7e5bb6d1494e38da08b515cb5
Jane <994824764@qq.com>
2023-07-10 16:38:40 +0800

    通行证 - 保存图片

--- miniprogram/visitor_pkg/pages/passport/index.js
+++ miniprogram/visitor_pkg/pages/passport/index.js
@@ -16,4 +16,19 @@ Page({
       imageUrl: 'https://enjoy-plus.oss-cn-beijing.aliyuncs.com/images/share_poster.png',
     }
   },
+  // 点击保存图片时会触发
+  async saveImage() {
+    // 得到网络图片的信息(得到一个临时路径)
+    const res = await wx.getImageInfo({
+      src: this.data.url
+    })
+    // 将图片保存到本地相册
+    await wx.saveImageToPhotosAlbum({
+      filePath: res.path,
+    })
+    wx.showToast({
+      title: '保存到相册',
+      icon: 'success'
+    })
+  }
 })
