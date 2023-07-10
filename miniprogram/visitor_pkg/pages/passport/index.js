d275dd8f26028a19c9da157658e734643b17c13c
Jane <994824764@qq.com>
2023-07-10 16:16:42 +0800

    通行证 - 数据渲染

--- miniprogram/visitor_pkg/pages/passport/index.js
+++ miniprogram/visitor_pkg/pages/passport/index.js
@@ -1,4 +1,14 @@
 Page({
+  onLoad({ id }) {
+    // 获取通行证
+    this.getPassport(id)
+  },
+  async getPassport(id) {
+    if (!id) return
+    const { code, data: passport } = await wx.http.get('/visitor/' + id)
+    if (code !== 10000) return wx.utils.toast('获取通行证失败!')
+    this.setData({ ...passport })
+  },
   onShareAppMessage() {
     return {
       title: '查看通行证',
